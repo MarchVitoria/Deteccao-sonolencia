bf32be26ed68e5c95ba01175ac342ace02935d89
Laura Lopes <laura.lopes@sistemafiep.org.com>
2023-08-04 11:26:53 -0300

    Implementando a detecção de landmarks

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # import the opencv library
 import cv2
 import face_detection
+import dlib
 
 def draw_faces(im, bboxes):
     for bbox in bboxes:
         x0, y0, x1, y1 = [int(_) for _ in bbox]
         cv2.rectangle(im, (x0, y0), (x1, y1), (0, 0, 255), 2)
 
-# Initialize detector
+# Initialize face detector
 detector = face_detection.build_detector("RetinaNetMobileNetV1", confidence_threshold=.5, nms_iou_threshold=.3)
+
+# Initialize landmarks detector
+predictor = dlib.shape_predictor("predictor.dat")
 
 # define a video capture object
 vid = cv2.VideoCapture(0)
@@ -19,28 +23,30 @@ while(True):
     # by frame
     ret, frame = vid.read()
 
-    #Detectar face
     # Detect faces from the image
-    # Note:
-    #   1. The input image must be a byte array of dimension HxWx3.
-    #   2. The return value is a Nx5 (for S3FD) or a Nx15 (for RetinaFace) matrix,
-    #      in which N is the number of detected faces. The first 4 columns store 
-    #      (in this order) the left, top, right, and bottom coordinates of the 
-    #      detected face boxes. The 5th columns stores the detection confidences.
-    #      The remaining columns store the coordinates (in the order of x1, y1, x2,
-    #      y2, ...) of the detected landmarks.
+    # This will return a tensor with shape [N, 5], 
+    # where N is number of faces and the five elements are [xmin, ymin, xmax, ymax, detection_confidence]
     detections = detector.detect(frame)[:, :4]
     draw_faces(frame, detections)
+    print("Number of faces detected: {}".format(len(detections)))
 
-    #Se existir face
-    #   Detectar landmark
+    if len(detections) > 0:
+        # Detect landmark
+        for k, d in enumerate(detections):
+            faceBoxRectangle = dlib.rectangle(left=d[0], top=d[1], right=d[2], bottom=d[3])
+            print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+                k, faceBoxRectangle.left(), faceBoxRectangle.top(), faceBoxRectangle.right(), faceBoxRectangle.bottom()))
+            
+            # Get the landmarks/parts for the face in box d.
+            shape = predictor(frame, faceBoxRectangle)
+            print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
+                                                    shape.part(1)))
+            # TODO Draw the face landmarks on the screen.
   
     # Display the resulting frame
     cv2.imshow('frame', frame)
       
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
+    # the 'q' button is set as the quitting button
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
   
