ed202aeebd3e60439c307611a7d3284d66d7f144
Laura Lopes <laura.lopes@sistemafiep.org.com>
2023-08-04 18:34:40 -0300

    Mudando parâmetros nu e cascade depth no treinamento

--- landmarks.py
+++ landmarks.py
@@ -48,9 +48,11 @@ options.oversampling_amount = 300
 # I'm also reducing the capacity of the model by explicitly increasing
 # the regularization (making nu smaller) and by using trees with
 # smaller depths.
-options.nu = 0.05
+options.nu = 0.1
 options.tree_depth = 2
 options.be_verbose = True
+options.num_threads = 12
+options.cascade_depth = 15
 
 # dlib.train_shape_predictor() does the actual training.  It will save the
 # final predictor to predictor.dat.  The input is an XML file that lists the
@@ -72,38 +74,3 @@ print("\nTraining accuracy: {}".format(
 testing_xml_path = os.path.join(faces_folder, "labels_ibug_300W_test.xml")
 print("Testing accuracy: {}".format(
     dlib.test_shape_predictor(testing_xml_path, "predictor.dat")))
-
-# Now let's use it as you would in a normal application.  First we will load it
-# from disk. We also need to load a face detector to provide the initial
-# estimate of the facial location.
-predictor = dlib.shape_predictor("predictor.dat")
-detector = dlib.get_frontal_face_detector()
-
-# Now let's run the detector and shape_predictor over the images in the faces
-# folder and display the results.
-print("Showing detections and predictions on the images in the faces folder...")
-win = dlib.image_window()
-for f in glob.glob(os.path.join(faces_folder, "*.jpg")):
-    print("Processing file: {}".format(f))
-    img = dlib.load_rgb_image(f)
-
-    win.clear_overlay()
-    win.set_image(img)
-
-    # Ask the detector to find the bounding boxes of each face. The 1 in the
-    # second argument indicates that we should upsample the image 1 time. This
-    # will make everything bigger and allow us to detect more faces.
-    dets = detector(img, 1)
-    print("Number of faces detected: {}".format(len(dets)))
-    for k, d in enumerate(dets):
-        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-            k, d.left(), d.top(), d.right(), d.bottom()))
-        # Get the landmarks/parts for the face in box d.
-        shape = predictor(img, d)
-        print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
-                                                  shape.part(1)))
-        # Draw the face landmarks on the screen.
-        win.add_overlay(shape)
-
-    win.add_overlay(dets)
-    dlib.hit_enter_to_continue()
